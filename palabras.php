c001e3973b98b72f1d8526b8cce640a62ecec84b
Ivan-PC\Ivan <kimagure44@yahoo.es>
2017-05-15 02:22:40 +0200

    Pequeño juego en Javascript

---
+++ palabras.php
@@ -0,0 +1,29 @@
+<?php
+        $letrasEmpieza = "ar";
+        $letrasTermina = "";
+        $palabrasQueTenganLetras = "";
+        $palabrasQueNoTenganLetras = "";
+        $palabrasQueTenganLaCadena = "";
+        $palabrasQueNoTenganLaCadena = "";
+        $numeroDeSilabas = 0;
+        $numeroDeLetras = 0;
+        $url = "http://www.palabrasque.com/buscador.php?i=".$letrasEmpieza;
+        $url.= "&f=".$letrasTermina."&tv=0";
+        $url.= "&button=Buscar+palabras&ms=".$palabrasQueTenganLetras;
+        $url.= "&mns=".$palabrasQueNoTenganLetras;
+        $url.= "&m=".$palabrasQueTenganLaCadena;
+        $url.= "&mn=".$palabrasQueNoTenganLaCadena;
+        $url.= "&fs=".$numeroDeSilabas;
+        $url.= "&fnl=".$numeroDeLetras;
+        $url.= "&fa=0&d=0";
+        $palabras = file_get_contents($url);
+        $r = explode("\n",$palabras);
+        $palabras = $r[223];
+        $td = "<td>";
+        $td2 = "</td>";
+        $ini = strpos($palabras, $td);
+        $fin = strpos($palabras, $td2);
+        $text = substr($palabras, $ini + strlen($td), $fin);
+        $words = explode(",",$text);
+        echo json_encode($words);
+?>
